40f57969e10f4f8dbea96c8ce942770e6ef2f350
Kevin Gautreau <contact@kgaut.net>
2019-02-06 12:18:42 +0100

    Define field type

--- src/Plugin/Field/FieldType/FaqQuestion.php
+++ src/Plugin/Field/FieldType/FaqQuestion.php
@@ -26,23 +26,17 @@ class FaqQuestion extends FieldItemBase {
   /**
    * {@inheritdoc}
    */
-  public static function defaultStorageSettings() {
-    return [
-      'max_length' => 255,
-      'is_ascii' => FALSE,
-      'case_sensitive' => FALSE,
-    ] + parent::defaultStorageSettings();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
-    // Prevent early t() calls by using the TranslatableMarkup.
-    $properties['value'] = DataDefinition::create('string')
-      ->setLabel(new TranslatableMarkup('Text value'))
-      ->setSetting('case_sensitive', $field_definition->getSetting('case_sensitive'))
+    $properties['question'] = DataDefinition::create('string')
+      ->setLabel(new TranslatableMarkup('Question'))
       ->setRequired(TRUE);
+
+    $properties['answer'] = DataDefinition::create('string')
+      ->setLabel(t('Answer'))
+      ->setRequired(TRUE);
+
+    $properties['format'] = DataDefinition::create('filter_format')
+      ->setLabel(t('Text format'));
 
     return $properties;
   }
@@ -53,11 +47,21 @@ class FaqQuestion extends FieldItemBase {
   public static function schema(FieldStorageDefinitionInterface $field_definition) {
     $schema = [
       'columns' => [
-        'value' => [
-          'type' => $field_definition->getSetting('is_ascii') === TRUE ? 'varchar_ascii' : 'varchar',
-          'length' => (int) $field_definition->getSetting('max_length'),
-          'binary' => $field_definition->getSetting('case_sensitive'),
+        'question' => [
+          'type' => 'varchar_ascii',
+          'length' => 255,
         ],
+        'answer' => [
+          'type' => 'text',
+          'size' => 'big',
+        ],
+        'answer_format' => [
+          'type' => 'varchar_ascii',
+          'length' => 255,
+        ],
+      ],
+      'indexes' => [
+        'format' => ['format'],
       ],
     ];
 
@@ -67,61 +71,20 @@ class FaqQuestion extends FieldItemBase {
   /**
    * {@inheritdoc}
    */
-  public function getConstraints() {
-    $constraints = parent::getConstraints();
-
-    if ($max_length = $this->getSetting('max_length')) {
-      $constraint_manager = \Drupal::typedDataManager()->getValidationConstraintManager();
-      $constraints[] = $constraint_manager->create('ComplexData', [
-        'value' => [
-          'Length' => [
-            'max' => $max_length,
-            'maxMessage' => t('%name: may not be longer than @max characters.', [
-              '%name' => $this->getFieldDefinition()->getLabel(),
-              '@max' => $max_length
-            ]),
-          ],
-        ],
-      ]);
-    }
-
-    return $constraints;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public static function generateSampleValue(FieldDefinitionInterface $field_definition) {
-    $random = new Random();
-    $values['value'] = $random->word(mt_rand(1, $field_definition->getSetting('max_length')));
+    $values['question'] = t('How are you ?');
+    $values['answer'] = t('I m fine');
+    $values['answer_format'] = 'full html';
     return $values;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function storageSettingsForm(array &$form, FormStateInterface $form_state, $has_data) {
-    $elements = [];
-
-    $elements['max_length'] = [
-      '#type' => 'number',
-      '#title' => t('Maximum length'),
-      '#default_value' => $this->getSetting('max_length'),
-      '#required' => TRUE,
-      '#description' => t('The maximum length of the field in characters.'),
-      '#min' => 1,
-      '#disabled' => $has_data,
-    ];
-
-    return $elements;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function isEmpty() {
-    $value = $this->get('value')->getValue();
-    return $value === NULL || $value === '';
+    $question = $this->get('question')->getValue();
+    $answer = $this->get('answer')->getValue();
+    return $question === NULL || $question === '' || $answer === NULL || $answer === '';
   }
 
 }
